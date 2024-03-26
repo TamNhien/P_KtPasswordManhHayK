0f91b8625edc34533c360e3409d50e184b8fba4e
TamNhien <canhthien91@gmail.com>
2024-03-26 20:31:53 +0700

    Add project files.

---
+++ P_KtPasswordManhHayK.py
@@ -0,0 +1,8 @@
+﻿import re
+
+password = input("Nhập mật khẩu : ")
+
+is_strong = lambda s: len(s) >= 8 and bool(re.search(r'[!@#$%^&*()_+{}\[\]:;<>,.?/\\]', s)) and bool(re.search(r'[A-Z]', s)) and bool(re.search(r'\d', s)) and bool(re.search(r'[a-z]', s))
+
+print("Đây là một mật khẩu mạnh." if is_strong(password) else "Đây không phải là một mật khẩu mạnh.")
+
